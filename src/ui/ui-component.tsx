09453348bc64f1c6c8657f1504d7923214c45af4
Otto-Vector <50682440+Otto-Vector@users.noreply.github.com>
2022-06-01 14:50:45 +0300

    add it all to heroku

--- src/ui/ui-component.tsx
+++ src/ui/ui-component.tsx
@@ -19,7 +19,7 @@ export const UiComponent: React.FC<OwnPropsType> = (
 
     const [ textStringValue, setInputValue ] = useState( defaultValInput )
     const numArray = wordToArrayOfNumbers( textStringValue )
-    const summary =toOneFibonacciDigit( numArray.reduce( ( a, b ) => a + b ))
+    const summary = toOneFibonacciDigit( numArray.reduce( ( a, b ) => a + b ) )
     const reverseArray = numArray.slice().reverse()
     const onSubmit = ( { title }: { title: string } ): void => {
         setInputValue( modificationToNormal( title ) )
@@ -27,15 +27,19 @@ export const UiComponent: React.FC<OwnPropsType> = (
 
     return (
         <div className={ styles.component }>
-            <h2 className={styles.header}>{ textStringValue  }</h2>
+            <h2 className={ styles.header }>{ textStringValue }</h2>
 
             <InputForm onSubmit={ onSubmit }/>
-            <SplitList label={ 'Обычное (-->) сужение'} inputValue={ numArray } infoText={textStringValue}/>
-            <SplitList label={ 'Обратное (<--) сужение' } inputValue={ reverseArray } infoText={textStringValue}/>
-            <SplitList label={ '-->.<-- сужение' } inputValue={ [...numArray,...reverseArray] } infoText={textStringValue}/>
-            <SplitList label={ '<--.--> сужение' } inputValue={ [...reverseArray,...numArray] } infoText={textStringValue}/>
-            <SplitList label={ '-->S<-- сужение' } inputValue={ [...numArray,summary,...reverseArray] } infoText={textStringValue}/>
-            <SplitList label={ '<--S--> сужение' } inputValue={ [...reverseArray,summary,...numArray] } infoText={textStringValue}/>
+            <SplitList label={ 'Обычное (-->) сужение' } inputValue={ numArray } infoText={ textStringValue }/>
+            <SplitList label={ 'Обратное (<--) сужение' } inputValue={ reverseArray } infoText={ textStringValue }/>
+            <SplitList label={ '-->.<-- сужение' } inputValue={ [ ...numArray, ...reverseArray ] }
+                       infoText={ textStringValue }/>
+            <SplitList label={ '<--.--> сужение' } inputValue={ [ ...reverseArray, ...numArray ] }
+                       infoText={ textStringValue }/>
+            <SplitList label={ '-->S<-- сужение' } inputValue={ [ ...numArray, summary, ...reverseArray ] }
+                       infoText={ textStringValue }/>
+            <SplitList label={ '<--S--> сужение' } inputValue={ [ ...reverseArray, summary, ...numArray ] }
+                       infoText={ textStringValue }/>
 
         </div>
     )
